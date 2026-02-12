0c958afd8cf5d216a9cd0955090576ffc8a69bac
Amadou <54441173+treodred@users.noreply.github.com>
2026-02-12 15:28:22 -0500

    Update sensor.py

--- esphome/components/trill_component/sensor.py
+++ esphome/components/trill_component/sensor.py
@@ -14,7 +14,7 @@ trill_ns = cg.esphome_ns.namespace("trill")
 TrillComponent = trill_ns.class_("TrillComponent", cg.PollingComponent, i2c.I2CDevice)
 
 # Trill device types enum
-TrillDevice = trill_ns.enum("device_type_", is_class=True)
+TrillDevice = trill_ns.enum("Device", is_class=True)
 #TrillDevice = trill_ns.enum("TrillDevice", is_class=True)
 TRILL_DEVICES = {
     "BAR": TrillDevice.TRILL_BAR,
@@ -204,6 +204,7 @@ async def to_code(config):
             sens = await sensor.new_sensor(button_config)
 
             cg.add(var.set_button_sensor(i, sens))
+
 
 
 
